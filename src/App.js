46da79af034c0a36b0e13e27efeb29384357fa7d
Cristian Abrante <cristianabrantedorta@gmail.com>
2019-01-19 12:43:04 +0000

    Ref: YOLO model loading refactored

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Yolo from './yolo/yolo';
 class App extends Component {
   yolo;
 
+  state = {
+    model: 'v3',
+    tiny: false,
+  };
+
   loadModel = () => {
-    this.yolo = new Yolo('v3');
+    this.yolo = new Yolo('v3', false);
     this.yolo.loadModel().then(
-        function() {
+        () => {
           console.log(this.yolo.model)
         }
     )
@@ -16,7 +21,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        This is the initial configuration of YOLO
         <input type="button" value="load model" onClick={this.loadModel}/>
       </div>
     );
--- src/yolo/yolo.js
+++ src/yolo/yolo.js
@@ -4,12 +4,30 @@ import Models from './config/model/';
 export default class Yolo {
   model;
   version;
+  isTiny;
 
-  constructor(version) {
+  constructor(version, isTiny = true) {
     this.version = version;
+    this.isTiny = isTiny;
   }
 
   loadModel = async () => {
-    this.model = await tf.loadModel(v3);
+    this.model = await tf.loadModel(this.getModelPath());
+  };
+
+  getModelPath = () => {
+    Yolo.checkModelConfig(this.version, this.isTiny);
+    return this.isTiny ? Models.tiny[this.version] : Models[this.version];
+  };
+
+  static checkModelConfig = (version, isTiny) => {
+    if (isTiny) {
+      if (Models.tiny[version] === undefined)
+        throw new Error(version + ' tiny is not available');
+    } else {
+      if (Models[version] === undefined) {
+        throw new Error(version + ' is not available');
+      }
+    }
   }
 }
