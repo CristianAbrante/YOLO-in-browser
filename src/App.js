909179e504a9a09a8f2995263ac7fd0a23bd1ee1
Cristian Abrante <cristianabrantedorta@gmail.com>
2019-01-20 19:58:47 +0000

    Ref: App and index.html refactored

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import React, { Component } from 'react';
-import CanvasManipulator from './image/CanvasManipulator';
-import ModelSelector from './components/model-selector/ModelSelector';
-import theme from './components/theme';
 
+// Components imports
+import theme from './components/theme';
+import NavigationBar from './components/navigation-bar/NavigationBar';
+import ViewsManager from './components/views/ViewsManager'
+
+// Material UI imports
 import {MuiThemeProvider} from '@material-ui/core/styles';
 
 class App extends Component {
   yolo;
-  inputRef;
-  canvasRef;
-  canvasManipulator;
-
   state = {
-    model: 'v3tiny',
+    viewsManager: new ViewsManager(this.yolo),
   };
 
-  constructor(props) {
-    super(props);
-    this.inputRef = React.createRef();
-    this.canvasRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.canvasManipulator = new CanvasManipulator(this.canvasRef.current);
-  }
+  updateView = () => {
+    this.setState({viewsManager: this.state.viewsManager});
+  };
 
   render() {
     return (
         <MuiThemeProvider theme={theme}>
           <div>
-            <ModelSelector
-                yolo={this.yolo}/>
+            <NavigationBar
+                viewsManager={this.state.viewsManager}
+                updateView={this.updateView}/>
+            {this.state.viewsManager.getCurrentViewComponent()}
           </div>
         </MuiThemeProvider>
     );
