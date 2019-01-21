37c685c9cf492389fb147162d001ec57c44971da
Cristian Abrante <cristianabrantedorta@gmail.com>
2019-01-21 12:52:28 +0000

    Addlocal images uploaded

--- src/components/views/image/ImageCarousel.js
+++ src/components/views/image/ImageCarousel.js
@@ -1,43 +1,18 @@
 import React, {Component} from 'react';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import {withStyles} from '@material-ui/core';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const tutorialSteps = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-        'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-        'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-        'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-        'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=255&q=60',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-        'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=255&q=60',
-  },
-];
+function importAll(r) {
+  let images = [];
+  r.keys().map(item => { images.push(r(item)); });
+  return images;
+}
+const images = importAll(require.context('./../../../yolo/test/images', false, /\.(png|jpe?g|svg)$/));
 
 const styles = {
   root: {
@@ -55,7 +30,7 @@ const styles = {
   img: {
     display: 'block',
     overflow: 'hidden',
-    maxHeight: '1000px',
+    maxHeight: '300px',
     maxWidth: '100%',
     margin: 'auto'
   },
@@ -80,18 +55,16 @@ class ImageCarousel extends Component{
 
   render() {
     let {classes} = this.props;
-    let maxSteps = tutorialSteps.length;
+    let maxSteps = images.length;
     return(
         <div className={classes.root}>
           <SwipeableViews
               index={this.state.activeStep}
               onChangeIndex={this.handleStepChange}
               enableMouseEvents>
-            {tutorialSteps.map((step, index) => (
-                <div key={step.label} >
-                  {Math.abs(this.state.activeStep - index) <= 2 ? (
-                      <img className={classes.img} src={step.imgPath} alt={step.label} />
-                  ) : null}
+            {images.map((image, index) => (
+                <div key={image} >
+                  <img className={classes.img} src={image} alt=""/>
                 </div>
             ))}
           </SwipeableViews>
