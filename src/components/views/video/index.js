da610279f05f5d9c0d43cd9eeaa9e4f7f53de611
Cristian Abrante <cristianabrantedorta@gmail.com>
2019-01-24 13:26:21 +0000

    Add: basic video UI built

--- src/components/views/video/index.js
+++ src/components/views/video/index.js
@@ -1,15 +1,47 @@
 import React, {Component} from 'react';
 
-import VideoTest from '../../../yolo/test/video/OUT.mp4';
+import VideoTest from '../../../yolo/test/video/video.mp4';
 import CanvasManager from '../../../image/CanvasVideoManipulator';
-import Yolo from '../../../yolo/yolo';
 import VideoManipulator from '../../../video/VideoManipulator';
+
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+import {withStyles} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography/Typography';
+import Divider from '@material-ui/core/Divider/Divider';
+import Dialog from '@material-ui/core/Dialog/Dialog';
+import CircularProgress
+  from '@material-ui/core/CircularProgress/CircularProgress';
+
+const styles = {
+  container: {
+    padding: 10,
+    height: '90vh',
+    display: 'grid',
+    gridTemplateRows: "60% 10%",
+  },
+  canvasContainer: {
+    display: 'flex',
+    width: '99vw',
+    background: '#D1D7DE',
+    overflow: 'auto'
+  },
+  buttonsContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
 
 class VideoView extends Component {
   canvas;
   video;
   canvasManager;
   videoManipulator;
+
+  state = {
+    dialogOpen: true,
+  };
 
   constructor(props) {
     super(props);
@@ -37,6 +69,7 @@ class VideoView extends Component {
           () => {
             console.log('Frames processed!');
             this.videoManipulator.stopPreprocess();
+            this.setState({dialogOpen: false});
           }
       )
     }
@@ -47,25 +80,49 @@ class VideoView extends Component {
   };
 
   render() {
+    let {classes} = this.props;
     return (
         <div>
-          <div>
-            <video
-                ref={this.video}
-                src={VideoTest}
-                controls
-                style={{display: 'none'}}
-                onPlay={this.onVideoPlayed}
-                onPause={this.onVideoPaused}
-                muted/>
+          <Dialog
+              open={this.state.dialogOpen}
+              fullWidth={true}>
+            <Paper align="center">
+              <Typography
+                  align="center"
+                  variant="overline">
+                Preprocessing video
+              </Typography>
+              <Divider/>
+              <CircularProgress color="secondary" style={{margin: 20}}/>
+            </Paper>
+          </Dialog>
+
+          <div className={classes.container}>
+            <Paper className={classes.canvasContainer}>
+              <canvas ref={this.canvas}>
+                Canvas not supported in your browser
+              </canvas>
+            </Paper>
+            <Paper className={classes.buttonsContainer}>
+              <video
+                  ref={this.video}
+                  src={VideoTest}
+                  controls
+                  style={{display: 'none'}}
+                  onPlay={this.onVideoPlayed}
+                  onPause={this.onVideoPaused}
+                  muted/>
+              <Fab
+                  onClick={this.playProcessed}
+                  variant='extended'
+                  color='secondary'>
+                Play video
+              </Fab>
+            </Paper>
           </div>
-          <div>
-            <canvas ref={this.canvas}></canvas>
-          </div>
-          <button onClick={this.playProcessed}>Play processed</button>
         </div>
     );
   }
 }
 
-export default VideoView;
+export default withStyles(styles)(VideoView);
