81c0ac59f78180c85c4bb2343974aeac08ce9120
Cristian Abrante <cristianabrantedorta@gmail.com>
2019-01-25 16:45:21 +0000

    App deployed

---
+++ build/precache-manifest.c77142680ad4d85c3b915ba5cbfe7260.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2d6a34273e203a6167b9051de75b46b",
+    "url": "/YOLO-in-browser/static/media/img1.c2d6a342.jpg"
+  },
+  {
+    "revision": "1bdeb2a71ae1bdc4ad03",
+    "url": "/YOLO-in-browser/static/css/main.6f010f25.chunk.css"
+  },
+  {
+    "revision": "4a89a74112ae2d677037",
+    "url": "/YOLO-in-browser/static/js/1.4a89a741.chunk.js"
+  },
+  {
+    "revision": "f786077fd8cd4107cf0e",
+    "url": "/YOLO-in-browser/static/js/runtime~main.f786077f.js"
+  },
+  {
+    "revision": "44f2d3cd282b51ee9c962b0f73157f77",
+    "url": "/YOLO-in-browser/static/media/video.44f2d3cd.mp4"
+  },
+  {
+    "revision": "0efe2b8fa0609cf67d33ad9ed8112e66",
+    "url": "/YOLO-in-browser/static/media/dog.0efe2b8f.jpg"
+  },
+  {
+    "revision": "1915b52cadf9a8fdba8ec5535c42c0a2",
+    "url": "/YOLO-in-browser/static/media/eagle.1915b52c.jpg"
+  },
+  {
+    "revision": "ebc4ffadb173aa8c539e1566d50c6ef4",
+    "url": "/YOLO-in-browser/static/media/giraffe.ebc4ffad.jpg"
+  },
+  {
+    "revision": "5aed3c9bd2754c34d870ba0fe5d924b2",
+    "url": "/YOLO-in-browser/static/media/herd_of_horses.5aed3c9b.jpg"
+  },
+  {
+    "revision": "1bdeb2a71ae1bdc4ad03",
+    "url": "/YOLO-in-browser/static/js/main.1bdeb2a7.chunk.js"
+  },
+  {
+    "revision": "8251d816b9da2f537de607d1ee79756b",
+    "url": "/YOLO-in-browser/static/media/img2.8251d816.jpg"
+  },
+  {
+    "revision": "627571037af4d914630f90e3ecede79f",
+    "url": "/YOLO-in-browser/static/media/img3.62757103.jpg"
+  },
+  {
+    "revision": "cac6270ecec9f6b69190ee56a61522b6",
+    "url": "/YOLO-in-browser/static/media/img4.cac6270e.jpg"
+  },
+  {
+    "revision": "ff5c271eb813ffd9f78a17cede0d40a3",
+    "url": "/YOLO-in-browser/static/media/messi.ff5c271e.jpg"
+  },
+  {
+    "revision": "9cd71ee8cd7788f143ec28a184bd4794",
+    "url": "/YOLO-in-browser/static/media/person.9cd71ee8.jpg"
+  },
+  {
+    "revision": "fb9ac31c5fdac6d77d021d6b3aad4105",
+    "url": "/YOLO-in-browser/static/media/scream.fb9ac31c.jpg"
+  },
+  {
+    "revision": "2e61088931053569ef72ce43de392ee3",
+    "url": "/YOLO-in-browser/static/media/street.2e610889.jpg"
+  },
+  {
+    "revision": "edf787139d77993cf407a5a4114c2484",
+    "url": "/YOLO-in-browser/index.html"
+  }
+];
